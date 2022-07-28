529db2f5b05df55e46e70f13666ee246e5497af2
Alon Noy <alon.noy@algosec.com>
2022-07-28 11:02:41 +0300

    changed

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
-FROM node:12-alpine AS builder
-WORKDIR /action
-COPY package*.json ./
-RUN npm ci
-COPY tsconfig*.json ./
-COPY src/ src/
-RUN npm run build \
-  && npm prune --production
+# FROM node:12-alpine AS builder
+# WORKDIR /action
+# COPY package*.json ./
+# RUN npm ci
+# COPY tsconfig*.json ./
+# COPY src/ src/
+# RUN npm run build \
+#   && npm prune --production
 FROM node:12-alpine
 RUN apk update
-RUN apk add --no-cache tini git bash
+RUN apk add tini git bash
 RUN mkdir -p /scripts
 COPY scripts/changedFiles.sh  /scripts
 WORKDIR /scripts
 RUN chmod +x changedFiles.sh
 RUN ./changedFiles.sh
-COPY --from=builder action/package.json .
-COPY --from=builder action/lib lib/
-COPY --from=builder action/node_modules node_modules/
+# COPY --from=builder action/package.json .
+# COPY --from=builder action/lib lib/
+# COPY --from=builder action/node_modules node_modules/
 ENTRYPOINT [ "/sbin/tini", "--", "node", "/lib/index.js" ]
--- scripts/changedFiles.sh
+++ scripts/changedFiles.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
           # echo "::set-output debug=$(if [ -z ${ALGOSEC_IS_DEBUG} ]; then echo false; else echo true; fi;)"
  # Pull Request
-          git fetch origin "${GITHUB_BASE_REF}" --depth=1
+          printenv 
+          echo "INPUT_GITHUB_BASE_REF: ${INPUT_GITHUB_BASE_REF}, INPUT_GITHUB_SHA: ${INPUT_GITHUB_SHA}"
+          git fetch origin "${INPUT_GITHUB_BASE_REF}" --depth=1
           # Get the list of all changed resources
-          diff_result=$(git diff --name-only "origin/${GITHUB_BASE_REF}" ${GITHUB_SHA} )
-          echo "Diff between origin/${GITHUB_BASE_REF} and ${GITHUB_SHA}"
+          diff_result=$(git diff --name-only "origin/${INPUT_GITHUB_BASE_REF}" ${INPUT_GITHUB_SHA} )
+          echo "Diff between origin/${INPUT_GITHUB_BASE_REF} and ${INPUT_GITHUB_SHA}"
 
           # Extract terraform's files
           terraform_files=$(echo $diff_result | tr -s '[[:space:]]' '\n' | grep -o '.*\.tf$')
@@ -25,5 +27,5 @@
           done
           matrix_output="$matrix_output ]}"
 
-          echo "Prepared working matrix: $matrix_output"
-          echo "::set-output name=matrix::${matrix_output}"
+          # echo "Prepared working matrix: $matrix_output"
+          # echo "::set-output name=matrix::${matrix_output}"
